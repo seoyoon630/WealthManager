9d8834f8e7503ffb95541d4cf032c152ad225a02
DESKTOP-HCN82IH\neobns <seoyoon630@gmail.com>
2021-04-22 16:14:51 +0900

    Robolectric 추가

--- app/src/androidTest/java/com/bri/wealthmanager/DataBaseTest.kt
+++
@@ -1,5 +0,0 @@
-package com.bri.wealthmanager
-
-class DataBaseTest {
-
-}
---
+++ app/src/test/java/com/bri/wealthmanager/DataBaseTest.kt
@@ -0,0 +1,27 @@
+package com.bri.wealthmanager
+
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment.application
+import org.robolectric.Shadows
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [28])
+class DataBaseTest {
+
+    @Test
+    fun start_mainActivity() {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario.onActivity { activity ->
+            val toasts: List<Toast> = Shadows.shadowOf(application).shownToasts
+            toasts.forEach { println(it) }
+        }
+    }
+}
